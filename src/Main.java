f5aba3ca92bbd9c49b3fc26af5ef920a18832bcd
Mihai Mihalache <mihai.g.mihalache@endava.com>
2020-03-03 18:39:01 +0200

    Java - replace [0-9]

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
 
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello 4 World!".replaceAll("[0-9]", ""));
+
+
     }
 }
