118dbae0b78f531e9909556a62397a80353498ee
Mihai Mihalache <mihai.g.mihalache@endava.com>
2020-03-03 18:39:59 +0200

    Java - replace \d

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello 4 World!".replaceAll("[0-9]", ""));
+        System.out.println("Hello 4 World!".replaceAll("\\d", ""));
 
 
     }
