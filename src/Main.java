3c496f2aff9df24d11e09db73c9f08b9dff74425
school4IT2020 <endavaschool123>
2020-03-04 10:39:56 +0200

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
 
-
-
-
     public static void main(String[] args) {
-        System.out.println("Hello 4 World!".replaceAll("\\d", ""));
-
-
+        System.out.println("School4IT is 123 fun!".replaceAll("\\d", ""));
     }
 }
