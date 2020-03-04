1d5835dad84090765ebf63e1164a9688a839ca6c
Mihai Mihalache <mihai.g.mihalache@endava.com>
2020-03-04 14:06:46 +0200

    Java - 6th Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("School4IT is 123 fun!".replaceAll("\\d", ""));
+        System.out.println("School4IT is 1234 fun!".replaceAll("\\d", ""));
     }
 }
