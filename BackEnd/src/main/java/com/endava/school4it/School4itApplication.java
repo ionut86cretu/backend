3b9255e29d98687ac3bbc0edd54ab1c4b7bb69b7
Mihai Mihalache <mihai.g.mihalache@endava.com>
2020-03-05 19:17:19 +0200

    Spring Boot Application

---
+++ BackEnd/src/main/java/com/endava/school4it/School4itApplication.java
@@ -0,0 +1,13 @@
+package com.endava.school4it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School4itApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School4itApplication.class, args);
+	}
+
+}
