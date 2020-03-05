748e10119b934d8cd4f9db515a3181e053038ef0
Mihai Mihalache <mihai.g.mihalache@endava.com>
2020-03-05 19:19:04 +0200

    Spring Boot Application

---
+++ BackEnd/src/test/java/com/endava/school4it/School4itApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.school4it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School4itApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
