74919e8df3e41f166e5f47ee532b5a1dd7e9a67b
Sorin Blaga <sorin.blaga@endava.com>
2020-04-09 14:39:18 +0300

    Session 2.1/2_components

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { TopicListComponent } from './components/topic-list/topic-list.component';
+import { TopicCardComponent } from './components/topic-card/topic-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    TopicListComponent,
+    TopicCardComponent
   ],
   imports: [
     BrowserModule,
