d53e42e6f7f1f5ed8f9161a7da633f0ee1445361
cbobocel <Cristian.Bobocel@endava.com>
2020-04-24 19:46:14 +0300

    2.4_1 comment service

--- FrontEnd/src/app/components/topic-card/topic-card.component.ts
+++ FrontEnd/src/app/components/topic-card/topic-card.component.ts
@@ -4,34 +4,34 @@ import { Router } from '@angular/router';
 import { TopicService } from '../../services/topic.service';
 
 @Component({
-  selector: 'mb-topic-card',
-  templateUrl: './topic-card.component.html',
-  styleUrls: ['./topic-card.component.scss']
+    selector: 'mb-topic-card',
+    templateUrl: './topic-card.component.html',
+    styleUrls: ['./topic-card.component.scss']
 })
 export class TopicCardComponent implements OnInit {
-  @Input() topic: TopicModel;
-  @Input() topicLinksActive: boolean = true;
+    @Input() topic: TopicModel;
+    @Input() topicLinksActive = true;
 
-  constructor(protected router: Router,
-              private topicService: TopicService) {
-  }
-
-  ngOnInit() {
-  }
-
-  goToDetailsPage() {
-    if (this.topicLinksActive) {
-      this.router.navigateByUrl('/topic-details/' + this.topic.id);
+    constructor(protected router: Router,
+                private topicService: TopicService) {
     }
-  }
 
-  vote(upVote: boolean) {
-    this.topicService.voteTopic(this.topic.id, upVote).subscribe(result => {
-      console.log('upVote:', upVote);
-      this.topic = result;
-    }, error => {
-      console.log('topic vote error', error);
+    ngOnInit() {
     }
-    );
-  }
+
+    goToDetailsPage() {
+        if ( this.topicLinksActive ) {
+            this.router.navigateByUrl('/topic-details/' + this.topic.id);
+        }
+    }
+
+    vote(upVote: boolean) {
+        this.topicService.voteTopic(this.topic.id, upVote).subscribe(result => {
+                console.log('upVote:', upVote);
+                this.topic = result;
+            }, error => {
+                console.log('topic vote error', error);
+            }
+        );
+    }
 }
